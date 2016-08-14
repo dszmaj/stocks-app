0a08825d8b70d0cf8dd2179a0c827a3bcd56f7e0
dszmaj <8*TebOFn#CpIpcY0UYPbwq4P6$cSJzCC>
2016-08-14 23:02:25 +0200

    limiting selection to 3 symbols

--- src/app/priceGraph/fullList.component.ts
+++ src/app/priceGraph/fullList.component.ts
@@ -2,11 +2,14 @@ import {
   OnInit,
   Output,
   Component,
+  ViewChild,
+  ElementRef,
   EventEmitter,
   ViewEncapsulation,
   ChangeDetectionStrategy,
 } from '@angular/core';
 import * as d3 from 'd3';
+import * as last from 'lodash.last';
 import { Observable } from 'rxjs';
 import { Http } from '@angular/http';
 import { StoreService } from '../shared/store.service';
@@ -17,13 +20,13 @@ import { StoreActions } from '../shared/store.actions';
 @Component({
   selector: 'full-list',
   host: {'class': 'col-md-2'},
-  styles: [`select {height: 400px;}`],
+  styles: [`select {height: 300px;}`],
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
   <h3>Symbols:</h3>
   <p><small>Select max. 3</small></p>
-  <select multiple class="form-control">
+  <select multiple class="form-control" #sel (click)="selectedOptions()">
     <option *ngFor="let symbol of symbols | async">{{ symbol }}</option>
   </select>
   `
@@ -31,6 +34,7 @@ import { StoreActions } from '../shared/store.actions';
 export class FullListComponent implements OnInit {
   private symbols: Observable<string[]> = this.store.observe$.map(state => state.symbols);
   @Output() currentSelection: EventEmitter<Object> = new EventEmitter();
+  @ViewChild('sel') select: ElementRef;
 
   constructor(
     private http: Http,
@@ -48,12 +52,18 @@ export class FullListComponent implements OnInit {
       .map(data => data.json())
       .do(data => this.store.dispatch(StoreActions.loadToStore(data)))
       .subscribe(data => this.currentSelection.emit(data));
+
     // hacked csv parsing
-    // for this to work CORS must be disabled in browser
     // normally I would create server endpoint that will download, parse, store and serve as JSON
-    let nasdaq = '//www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download';
     Observable
-      .bindCallback(d3.csv)(nasdaq)
+      .bindCallback(d3.csv)('nasdaq.csv')
       .subscribe(data => this.store.dispatch(StoreActions.loadSymbols(data[1])));
+  }
+
+  selectedOptions() {
+    let selections = this.select.nativeElement.selectedOptions;
+    while (selections.length > 3) {
+      last(selections).selected = false;
+    }
   }
 }
