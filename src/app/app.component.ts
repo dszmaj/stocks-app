c6601a0999881530c0a11edcc102069c7645b57a
dszmaj <8*TebOFn#CpIpcY0UYPbwq4P6$cSJzCC>
2017-03-18 17:28:21 +0100

    initially working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import '../style/app.scss';
-
 
 @Component({
   selector: '[my-app]',
@@ -10,7 +8,12 @@ import '../style/app.scss';
     <div class="container">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <button type="button" 
+          class="navbar-toggle collapsed" 
+          data-toggle="collapse" 
+          data-target="#bs-example-navbar-collapse-1" 
+          aria-expanded="false"
+        >
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
